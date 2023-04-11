aafaae207c51407879ebc770b0181bee1ecbd1de
orenanbarros <renanalmeidadebarros@gmail.com>
2023-04-11 15:10:57 -0300

    acerto no envio de email, para fechar a modal apos 1 segundo e meio, caso o usuario nao clique no botao de ok

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -9,25 +9,26 @@ import Modal from './Modal';
 function ContactForm() {
     
     const [message , setMessage] = useState("")
-    const [closeModal, setCloseModal] = useState(true)
+    const [closeModal, setCloseModal] = useState(false)
     
     const sendEmail = (e:FormEvent<HTMLFormElement>)=>{
         e.preventDefault();     
-        
-        setMessage("Obrigado pelo seu contato !")
-        setCloseModal!(!close) 
-       
-        
+
         emailjs.sendForm('service_au5rcbh','template_jt1g49u',e.currentTarget,'qz2fHNuRySDsjmk0b')
         .then(function(response) {
             //console.log('SUCCESS!', response.status, response.text);
             setMessage("Obrigado pelo seu contato !")
             setCloseModal(!closeModal)
+            setTimeout((e:FormEvent<HTMLFormElement>) => {            
+                setCloseModal(false)
+            }, 1500);
+            e.currentTarget.reset()
+            
         }, function(error) {
             //console.log('FAILED...', error);
             setMessage("Opss, houve algum erro ao enviar sua mensagem")
             setCloseModal(!closeModal)
-        });
+        })
         
     }
     
@@ -42,9 +43,8 @@ function ContactForm() {
                 <textarea id="message" name="message" placeholder="Digite sua mensagem" rows={4} required/>
                 <button type="submit" > Enviar </button>
             </form>
-            {message &&(
-                <Modal text={message} buttonOk={true} close={closeModal} setCloseModal={setCloseModal}/>
-            )}
+            <Modal text={message} buttonOk={true} close={closeModal} setCloseModal={setCloseModal}/>
+            
         </div>
     )
 }
